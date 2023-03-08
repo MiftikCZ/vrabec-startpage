121f606643053fcea45caa9e4b57b461015bd1f4
MiftikCZ <memertv17@gmail.com>
2023-03-08 15:33:02 +0100

    some fixes in git

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "./"
+  base: "./",
+  build: {
+    outDir: "docs"
+  }
 })
